9c80520b6bb79bd2a16afc2a0419839f216c5acd
Jamie Taylor <jamie.taylor@rjj-software.co.uk>
2024-03-18 17:02:05 +0000

    Initial commit of "working" command line parsing

--- src/dotnet.podcasts.console/Program.cs
+++ src/dotnet.podcasts.console/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+
+namespace dotnet.podcasts.console
+{
+    public class Program
+    {
+        [Verb("create", HelpText = "Used to create a podcast project file")]
+        public class CreateOptions
+        {
+            
+        }
+
+        public static int RunAddAndReturnExitCode(CreateOptions opts)
+        {
+            return 0;
+        }
+
+        public static int HandleErrors(IEnumerable<Error> errors)
+        {
+            // TODO log all the errors
+            return 1;
+        }
+
+        public static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<CreateOptions>(args)
+                .MapResult(
+                    (CreateOptions opts) => RunAddAndReturnExitCode(opts),
+                    errs => HandleErrors(errs));
+        }
+    }
+}
